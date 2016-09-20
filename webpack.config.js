bf60e855f2ba67624c2f9c5269ca6953bce6604f
Stambula <lussenok07@gmail.com>
2016-09-20 13:24:28 +0300

    webpack.config added, create separate js folder

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+
+module.exports = {
+	context: path.resolve('js'),
+	entry: './js/script.es6',
+	output: {
+		filename: 'bundle.js'
+	},
+
+	watch: true,
+
+
+	module: {
+		loaders:[
+			{
+				test: /\.es6$/,
+				exclude: /node_modules/,
+				loader:"babel-loader",
+				query: {
+			        presets: ['es2015']
+			     }
+			}
+		]
+	},
+
+	resolve:{
+		extensions: ['', '.js', '.es6']
+	}
+}
